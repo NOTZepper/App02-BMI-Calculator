4f03d0d98ef9280b64085dd660547ddef64f7ea9
NOTZepper <114736407+NOTZepper@users.noreply.github.com>
2023-03-02 16:51:47 +0000

    Integrating App01

--- BMI Calculator/BMI Calculator/Program.cs
+++ BMI Calculator/BMI Calculator/Program.cs
@@ -6,6 +6,173 @@ namespace BMICalculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the program! Please choose an option:");
+            Console.WriteLine("1. Distance converter");
+            Console.WriteLine("2. BMI calculator");
+
+            string input = Console.ReadLine();
+            int choice;
+
+            while (!int.TryParse(input, out choice) || choice < 1 || choice > 2)
+            {
+                Console.WriteLine("Invalid choice. Please choose again.");
+                input = Console.ReadLine();
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    DistanceConverter();
+                    break;
+                case 2:
+                    BmiCalculator();
+                    break;
+            }
+
+            Console.WriteLine("Would you like to use the program again?");
+            Console.WriteLine("1. Yes");
+            Console.WriteLine("2. No");
+
+            input = Console.ReadLine();
+
+            while (input != "1" && input != "2")
+            {
+                Console.WriteLine("Invalid choice. Please choose again.");
+                input = Console.ReadLine();
+            }
+
+            if (input == "1")
+            {
+                Main(args);
+            }
+        }
+
+        static void DistanceConverter()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Distance Converter");
+            Console.WriteLine("By Fudail Khan");
+            Console.WriteLine("");
+
+            Console.WriteLine("Select unit to convert from:");
+            Console.WriteLine("1. Miles");
+            Console.WriteLine("2. Feet");
+            Console.WriteLine("3. Meters");
+            Console.WriteLine("");
+
+            int fromUnit;
+            while (true)
+            {
+                Console.Write("Enter your choice (1-3): ");
+                if (!int.TryParse(Console.ReadLine(), out fromUnit) || fromUnit < 1 || fromUnit > 3)
+                {
+                    Console.WriteLine("Invalid choice. Please enter a number between 1 and 3.");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("");
+
+            Console.WriteLine("Select unit to convert to:");
+            Console.WriteLine("1. Miles");
+            Console.WriteLine("2. Feet");
+            Console.WriteLine("3. Meters");
+            Console.WriteLine("");
+
+            int toUnit;
+            while (true)
+            {
+                Console.Write("Enter your choice (1-3): ");
+                if (!int.TryParse(Console.ReadLine(), out toUnit) || toUnit < 1 || toUnit > 3)
+                {
+                    Console.WriteLine("Invalid choice. Please enter a number between 1 and 3.");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("");
+
+            Console.Write("Enter the distance: ");
+            double distance;
+            while (!double.TryParse(Console.ReadLine(), out distance))
+            {
+                Console.WriteLine("Invalid distance. Please enter a valid number.");
+                Console.Write("Enter the distance: ");
+            }
+            Console.WriteLine("");
+
+            string fromUnitStr = "";
+            string toUnitStr = "";
+            double convertedDistance = 0;
+
+            switch (fromUnit)
+            {
+                case 1:
+                    fromUnitStr = "miles";
+                    switch (toUnit)
+                    {
+                        case 1:
+                            toUnitStr = "miles";
+                            convertedDistance = distance;
+                            break;
+                        case 2:
+                            toUnitStr = "feet";
+                            convertedDistance = distance * 5280;
+                            break;
+                        case 3:
+                            toUnitStr = "meters";
+                            convertedDistance = distance * 1609.34;
+                            break;
+                    }
+                    break;
+                case 2:
+                    fromUnitStr = "feet";
+                    switch (toUnit)
+                    {
+                        case 1:
+                            toUnitStr = "miles";
+                            convertedDistance = distance / 5280;
+                            break;
+                        case 2:
+                            toUnitStr = "feet";
+                            convertedDistance = distance;
+                            break;
+                        case 3:
+                            toUnitStr = "meters";
+                            convertedDistance = distance / 3.281;
+                            break;
+                    }
+                    break;
+                case 3:
+                    fromUnitStr = "meters";
+                    switch (toUnit)
+                    {
+                        case 1:
+                            toUnitStr = "miles";
+                            convertedDistance = distance / 1609.34;
+                            break;
+                        case 2:
+                            toUnitStr = "feet";
+                            convertedDistance = distance * 3.281;
+                            break;
+                        case 3:
+                            toUnitStr = "meters";
+                            convertedDistance = distance;
+                            break;
+                    }
+                    break;
+            }
+
+            Console.WriteLine("{0} {1} is equal to {2} {3}.", distance, fromUnitStr, convertedDistance, toUnitStr);
+            Console.ReadLine();
+        }
+
+        static void BmiCalculator()
+        {
             Console.WriteLine("BMI Calculator");
             Console.WriteLine("---------------");
             Console.WriteLine("This application will calculate your Body Mass Index (BMI) and provide your WHO weight status.");
